e7b20823e1bcd0f4a810cb87d16a6f923994b70e
Brijesh1990 <bkpandey.pandey@gmail.com>
2025-04-10 17:48:30 +0530

    10-04-2025 all data uploaded

---
+++ reactjs/module-2-essentials-javascript/4-terminal.js
@@ -0,0 +1,7 @@
+// a=10;
+// b=20;
+// c=a+b;
+// console.log(c)
+
+name="Hello Rahul"
+console.log(name)
---
+++ reactjs/module-2-essentials-javascript/js/alert.js
@@ -0,0 +1,2 @@
+alert('hey i am just load google')
+window.location='https://www.google.com'
